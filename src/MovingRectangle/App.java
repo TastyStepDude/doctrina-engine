24b69d62bf0144ef241673897be9923f2eef3994
Zach <zach160jg@gmail.com>
2024-09-18 13:48:31 -0400

    feat: amazing new game

---
+++ src/MovingRectangle/App.java
@@ -0,0 +1,10 @@
+package MovingRectangle;
+
+import Doctrina.Game;
+
+public class App {
+    public static void main(String[] args) {
+        Game game = new MovingRectangleGame();
+        game.start();
+    }
+}
