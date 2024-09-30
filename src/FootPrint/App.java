9aa492c478b50a94d9cb46b3e256d564a29052f1
Zach <zach160jg@gmail.com>
2024-09-30 09:23:17 -0400

    feat: footprints

--- src/FootPrint/App.java
+++ src/FootPrint/App.java
@@ -4,7 +4,7 @@ import Doctrina.Game;
 
 public class App {
     public static void main(String[] args) {
-        Game game = new FootPrintGame();
+        Game game = new FootprintGame();
         game.start();
     }
 }
